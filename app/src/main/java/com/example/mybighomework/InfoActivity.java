ae8717951b24c6edc844a4a083c50e4bc74e8163
lgh <128457626@qq.com>
2024-06-04 00:53:54 +0800

    240603_pibb

--- app/src/main/java/com/example/mybighomework/InfoActivity.java
+++ app/src/main/java/com/example/mybighomework/InfoActivity.java
@@ -7,6 +7,6 @@ public class InfoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info);
+        setContentView(R.layout.activity_info);//
     }
 }
