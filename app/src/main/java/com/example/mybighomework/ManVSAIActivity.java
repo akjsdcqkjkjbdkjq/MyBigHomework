b607a7d07549e5dc767b78855e05a7d88435ea4a
lgh <128457626@qq.com>
2024-06-03 19:44:05 +0800

    240603_rollbackfunc1

--- app/src/main/java/com/example/mybighomework/ManVSAIActivity.java
+++ app/src/main/java/com/example/mybighomework/ManVSAIActivity.java
@@ -4,7 +4,6 @@ import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
